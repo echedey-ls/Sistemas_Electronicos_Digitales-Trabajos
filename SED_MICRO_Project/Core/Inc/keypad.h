90f54fb96108ed2f3b4ddf73e40102a568d70efa
Lucas Herrera <127339929+LuHer10@users.noreply.github.com>
2024-01-03 20:28:48 +0100

    Changed getKey()

--- SED_MICRO_Project/Core/Inc/keypad.h
+++ SED_MICRO_Project/Core/Inc/keypad.h
@@ -12,13 +12,13 @@
 
 #define BANK GPIOD
 
-extern uint16_t outall;
+uint16_t outall = GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
 
-extern uint16_t inps[4];
-extern uint16_t outs[4];
+uint16_t inps[] = {GPIO_PIN_0, GPIO_PIN_1, GPIO_PIN_2, GPIO_PIN_3};
+uint16_t outs[] = {GPIO_PIN_4, GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_7};
 
-extern const char pads[17];
+//uint16_t getKey();
 
-void getKey(char* str);
+int getKey();
 
 #endif /* INC_KEYPAD_H_ */
