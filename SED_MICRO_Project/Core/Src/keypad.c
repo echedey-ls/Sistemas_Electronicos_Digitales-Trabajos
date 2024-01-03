60cd998eca14fd290fb90f2467f02cfc7ec1b073
Lucas Herrera <127339929+LuHer10@users.noreply.github.com>
2024-01-03 20:29:33 +0100

    Changed getKey()

--- SED_MICRO_Project/Core/Src/keypad.c
+++ SED_MICRO_Project/Core/Src/keypad.c
@@ -7,18 +7,6 @@
 
 #include "keypad.h"
 
-uint16_t outall = GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
-
-uint16_t inps[] = {GPIO_PIN_0, GPIO_PIN_1, GPIO_PIN_2, GPIO_PIN_3};
-uint16_t outs[] = {GPIO_PIN_4, GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_7};
-
-const char pads[] = {'\0',
-		'1', '4', '7', '*',
-		'2', '5', '8', '0',
-		'3', '6', '9', '#',
-		'A', 'B', 'C', 'D'
-};
-
 /*
 uint16_t getKey(){
 	uint16_t btns = 0;
@@ -32,7 +20,8 @@ uint16_t getKey(){
 }
 */
 
-void getKey(char* str){
+int getKey(){
+	static int pre_btn = 0;
 	int btns = 0;
 	for(int i = 0; i < 4; i++){
 		HAL_GPIO_WritePin(BANK, outs[i], GPIO_PIN_SET);
@@ -41,6 +30,10 @@ void getKey(char* str){
 			if(HAL_GPIO_ReadPin(BANK, inps[j])) btns = i*4 + j + 1;
 		}
 	}
-	str[0] = pads[btns];
-	str[1] = '\0';
+	if(btns == pre_btn) return 0;
+	else {
+		pre_btn = btns;
+		return btns;
+	}
+
 }
