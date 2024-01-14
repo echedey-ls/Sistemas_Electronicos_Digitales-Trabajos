55d68213743c48aa4c02fe8fa8ed0a98a23b720d
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-01-14 23:29:35 +0100

    Isolate command input from uart into an entity

--- SED_FPGA_Project/proyecto/proyecto.srcs/sources_1/FSM0.vhd
+++ SED_FPGA_Project/proyecto/proyecto.srcs/sources_1/FSM0.vhd
@@ -9,20 +9,26 @@ USE WORK.MACHINE_COMMON.ALL;
 
 ENTITY FSM0 IS
     GENERIC (
-        g_CLK_FREQ : POSITIVE := 100_000_000;
-        g_UART_FREQ : POSITIVE := 10_000
+        g_CLK_FREQ : POSITIVE := 100_000_000
     );
     PORT (
         i_CLK : IN STD_ULOGIC;
         i_RESET_N : IN STD_ULOGIC;
+
         i_CANCEL_BTN : IN STD_ULOGIC;
-        i_SERIAL_IN : IN STD_ULOGIC;
-        o_SERIAL_OUT : OUT STD_ULOGIC;
-        o_RX_BRK_LED : OUT STD_ULOGIC;
         o_HEATER : OUT STD_ULOGIC;
+        -- UART TELECOMS IO
+        i_cmd_received : IN STD_ULOGIC;
+        i_cmd_cancel : IN STD_ULOGIC;
+        i_cmd_product : IN STD_ULOGIC;
+        i_product_type : IN ProductType;
+        i8_converted_secs : IN BYTE;
+
+        o_status : OUT MachineStatus;
+        o_status_send : OUT STD_ULOGIC;
+        -- DISPLAY OUTPUT
         o8_REMAINING_SECS : OUT STD_ULOGIC_VECTOR(7 DOWNTO 0);
-        o_PRODUCT_STR : OUT ProductType;
-        oo_UART_DBG : OUT STD_ULOGIC_VECTOR
+        o_PRODUCT_STR : OUT ProductType
     );
 END FSM0;
 
@@ -38,57 +44,10 @@ ARCHITECTURE arch_fsm OF FSM0 IS
 
     -- Transition signals
     -- Composed later on
-    SIGNAL Timer_has_finished : STD_ULOGIC := '0'; -- Timer_not_zero = '0'
-    SIGNAL CMD_Cancel : STD_ULOGIC := '0';
-    SIGNAL Recv_CMD_Cancel : STD_ULOGIC := '0'; -- Recv_flag & CMD_Cancel
-    SIGNAL Recv_CMD_Product_Request : STD_ULOGIC := '0'; -- Recv_flag & !CMD_Cancel
-    SIGNAL Any_CMD_Cancel : STD_ULOGIC := '0'; -- takes into account cancel button (or gate)
-
-    --! UART
-    COMPONENT fluart IS -- Credits to https://github.com/marcj71/fluart
-        GENERIC (
-            CLK_FREQ : INTEGER := 50_000_000; -- main frequency (Hz)
-            SER_FREQ : INTEGER := 115200; -- bit rate (bps), any number up to CLK_FREQ / 2
-            BRK_LEN : INTEGER := 10 -- break duration (tx), minimum break duration (rx) in bits
-        );
-        PORT (
-            clk : IN STD_ULOGIC;
-            reset : IN STD_ULOGIC;
-
-            rxd : IN STD_ULOGIC;
-            txd : OUT STD_ULOGIC;
-
-            tx_data : IN STD_ULOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-            tx_req : IN STD_ULOGIC;
-            tx_brk : IN STD_ULOGIC;
-            tx_busy : OUT STD_ULOGIC;
-            tx_end : OUT STD_ULOGIC;
-            rx_data : OUT STD_ULOGIC_VECTOR(7 DOWNTO 0);
-            rx_data_valid : OUT STD_ULOGIC;
-            rx_brk : OUT STD_ULOGIC;
-            rx_err : OUT STD_ULOGIC
-        );
-    END COMPONENT fluart;
-    -- Signals
-    SIGNAL Recv_flag : STD_ULOGIC := '0';
-    SIGNAL Recv_data : STD_ULOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL Recv_brk : STD_ULOGIC := '0';
-    SIGNAL Send_flag_pulse : STD_ULOGIC := '0';
-    SIGNAL Send_status_enum : MachineStatus := AVAILABLE;
-    SIGNAL Send_status_byte : STD_ULOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-
-    --! INPUT COMMANDS TRANSLATOR
-    COMPONENT UART_RX_COMMAND_TRANSLATION IS
-        PORT (
-            i8_rx_code : IN BYTE;
-            o_IS_CANCEL_CMD : INOUT STD_ULOGIC;
-            o_product_type : OUT ProductType;
-            o8_converted_secs : OUT BYTE
-        );
-    END COMPONENT UART_RX_COMMAND_TRANSLATION;
-    -- Signals
-    SIGNAL CMD_RX_seconds : BYTE := (OTHERS => '0');
-    SIGNAL CMD_RX_prod_type : ProductType := NONE;
+    SIGNAL int_timer_finished : STD_ULOGIC := '0'; -- Timer_not_zero = '0'
+    SIGNAL int_RX_CMD_Cancel : STD_ULOGIC := '0'; -- i_cmd_received & i_cmd_cancel
+    SIGNAL int_RX_CMD_Product : STD_ULOGIC := '0'; -- i_cmd_received & i_cmd_product
+    SIGNAL int_any_CMD_Cancel : STD_ULOGIC := '0'; -- takes into account cancel button (or gate)
 
     --! COUNTDOWN TIMER
     COMPONENT e20_updown_cntr IS
@@ -107,11 +66,11 @@ ARCHITECTURE arch_fsm OF FSM0 IS
         );
     END COMPONENT e20_updown_cntr;
     -- Signals
-    SIGNAL Timer_not_zero : STD_ULOGIC := '0';
-    SIGNAL Timer_enable : STD_ULOGIC := '0';
-    SIGNAL Timer_clear : STD_ULOGIC := '0';
-    SIGNAL Timer_load : STD_ULOGIC := '0';
-    SIGNAL Timer_remaining : STD_ULOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL int_timer_not_zero : STD_ULOGIC := '0';
+    SIGNAL int_timer_enable : STD_ULOGIC := '0';
+    SIGNAL int_timer_clear : STD_ULOGIC := '0';
+    SIGNAL int_timer_load : STD_ULOGIC := '0';
+    SIGNAL int_timer_remaining : STD_ULOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
 
     --! FREQUENCY DIVIDER - IMPULSE GENERATOR
     -- To run the countdown timer at 1Hz
@@ -130,7 +89,7 @@ ARCHITECTURE arch_fsm OF FSM0 IS
     SIGNAL Do_countdown : STD_ULOGIC := '0';
     -- SIGNAL Last_prod_cancelled : STD_ULOGIC := '0';
 
-    --! Buttons Edge Detector & UART TX Send pulse
+    --! Buttons Edge Detector
     COMPONENT EDGEDTCTR IS
         PORT (
             CLK, RST_N : IN STD_ULOGIC;
@@ -140,62 +99,23 @@ ARCHITECTURE arch_fsm OF FSM0 IS
     END COMPONENT EDGEDTCTR;
     -- Signals
     SIGNAL CANCEL_BTN_EDGE : STD_ULOGIC := '0';
-    SIGNAL UART_TX_Send_flag : STD_ULOGIC := '0';
 BEGIN ----------------------------------------
-    --! Check inputs
-    ASSERT oo_UART_DBG'LENGTH >= 10;
-    --! UART instantiation
-    Inst00_uart : fluart
-    GENERIC MAP(
-        CLK_FREQ => g_CLK_FREQ,
-        SER_FREQ => g_UART_FREQ
-    )
-    PORT MAP(
-        clk => i_CLK,
-        reset => NOT i_RESET_N,
-
-        rxd => i_SERIAL_IN,
-        txd => o_SERIAL_OUT,
-
-        tx_data => Send_status_byte,
-        tx_req => Send_flag_pulse,
-        tx_brk => '0',
-        tx_busy => OPEN,
-        tx_end => OPEN,
-
-        rx_data => Recv_data,
-        rx_data_valid => Recv_flag,
-        rx_brk => Recv_brk,
-        rx_err => OPEN
-    );
-    Send_status_byte <= MachineStatus2Byte(Send_status_enum);
-    o_RX_BRK_LED <= Recv_brk;
-
-    --! Split UART RX data into Product Time & Product Type
-    Inst00_CMD_RX_CONVERTER : UART_RX_COMMAND_TRANSLATION
-    PORT MAP(
-        i8_rx_code => Recv_data,
-        o_IS_CANCEL_CMD => CMD_Cancel,
-        o_product_type => CMD_RX_prod_type,
-        o8_converted_secs => CMD_RX_seconds
-    );
-
     --! Countdown timer instantiation
     Inst00_CountDown : e20_updown_cntr
     GENERIC MAP(
         WIDTH => 8
     )
     PORT MAP(
-        CLR_N => i_RESET_N AND NOT Timer_clear,
+        CLR_N => i_RESET_N AND NOT int_timer_clear,
         CLK => i_CLK,
         UP => '0',
-        CE_N => NOT Timer_enable,
-        LOAD_N => NOT Timer_load,
-        J => CMD_RX_seconds,
-        ZERO_N => Timer_not_zero,
-        Q => Timer_remaining -- Output
+        CE_N => NOT int_timer_enable,
+        LOAD_N => NOT int_timer_load,
+        J => i8_converted_secs,
+        ZERO_N => int_timer_not_zero,
+        Q => int_timer_remaining -- Output
     );
-    o8_REMAINING_SECS <= Timer_remaining;
+    o8_REMAINING_SECS <= int_timer_remaining;
 
     --! Freq. generator / impulses instantiation
     Inst00_divider : e21_fdivider
@@ -206,7 +126,7 @@ BEGIN ----------------------------------------
         RESET => NOT i_RESET_N,
         CLK => i_CLK,
         CE_IN => Do_countdown,
-        CE_OUT => Timer_enable
+        CE_OUT => int_timer_enable
     );
 
     --! Cancel Input
@@ -218,22 +138,13 @@ BEGIN ----------------------------------------
         EDGE => CANCEL_BTN_EDGE
     );
 
-    Inst01_UART_TX_REQ_PULSE : EDGEDTCTR
-    PORT MAP(
-        CLK => i_CLK,
-        RST_N => i_RESET_N,
-        SYNC_IN => UART_TX_Send_flag,
-        EDGE => Send_flag_pulse
-    );
     --! State Machine !--
     ---------------------
     -- Transition signals
-    Timer_has_finished <= NOT Timer_not_zero;
-    Recv_CMD_Cancel <= Recv_flag AND CMD_Cancel;
-    Recv_CMD_Product_Request <= Recv_flag AND NOT CMD_Cancel;
-    Any_CMD_Cancel <= Recv_CMD_Cancel OR CANCEL_BTN_EDGE; -- UART Cancel Command OR Cancel Button
-
-    oo_UART_DBG(13 DOWNTO 10) <= Timer_has_finished & Recv_CMD_Cancel & Recv_CMD_Product_Request & Any_CMD_Cancel;
+    int_timer_finished <= NOT int_timer_not_zero;
+    int_RX_CMD_Cancel <= i_cmd_received AND i_cmd_cancel;
+    int_RX_CMD_Product <= i_cmd_received AND i_cmd_product;
+    int_any_CMD_Cancel <= int_RX_CMD_Cancel OR CANCEL_BTN_EDGE; -- UART Cancel Command OR Cancel Button
 
     -- Transition & actions process
     fsm_main_proc : PROCESS (i_RESET_N, i_CLK)
@@ -252,23 +163,23 @@ BEGIN ----------------------------------------
             -- See FSM diagram
             CASE CURRENT_STATE IS
                 WHEN ENTRY_POINT =>
-                    IF Recv_CMD_Product_Request = '1' THEN
+                    IF int_RX_CMD_Product = '1' THEN
                         NEXT_STATE := PRELAUNCH;
                     END IF;
                 WHEN PRELAUNCH =>
                     NEXT_STATE := COUNT_DOWN;
                 WHEN COUNT_DOWN =>
-                    IF Timer_has_finished = '1' THEN
+                    IF int_timer_finished = '1' THEN
                         NEXT_STATE := ORDER_FINISHED;
-                    ELSIF Any_CMD_Cancel = '1' THEN
+                    ELSIF int_any_CMD_Cancel = '1' THEN
                         NEXT_STATE := ORDER_CANCELLED;
                     END IF;
                 WHEN ORDER_CANCELLED =>
-                    IF Recv_CMD_Product_Request = '1' THEN
+                    IF int_RX_CMD_Product = '1' THEN
                         NEXT_STATE := PRELAUNCH;
                     END IF;
                 WHEN ORDER_FINISHED =>
-                    IF Recv_CMD_Product_Request = '1' THEN
+                    IF int_RX_CMD_Product = '1' THEN
                         NEXT_STATE := PRELAUNCH;
                     END IF;
                 WHEN OTHERS =>
@@ -278,47 +189,46 @@ BEGIN ----------------------------------------
             --! Action switch case
             -- Default action values
             o_HEATER <= '0';
-            Timer_load <= '0';
-            Timer_clear <= '0';
+            int_timer_load <= '0';
+            int_timer_clear <= '0';
             Do_countdown <= '0';
-            UART_TX_Send_flag <= '0';
-            Send_status_enum <= Send_status_enum;
+            o_status_send <= '0';
             int_PRODUCT_TYPE := int_PRODUCT_TYPE;
             CASE CURRENT_STATE IS
                 WHEN ENTRY_POINT =>
                     -- Show empty status on 7-Segments
                     int_PRODUCT_TYPE := DASHES;
                     -- Send AVAILABLE code on power-up / reset
-                    Send_status_enum <= AVAILABLE;
-                    UART_TX_Send_flag <= '1';
+                    o_status <= AVAILABLE;
+                    o_status_send <= '1';
                 WHEN PRELAUNCH =>
-                    Timer_load <= '1';
+                    int_timer_load <= '1';
                     -- Show status on 7-Segments
-                    int_PRODUCT_TYPE := CMD_RX_prod_type;
+                    int_PRODUCT_TYPE := i_product_type;
                     -- Send BUSY code on start
-                    UART_TX_Send_flag <= '1';
-                    Send_status_enum <= BUSY;
+                    o_status_send <= '1';
+                    o_status <= BUSY;
                 WHEN COUNT_DOWN =>
                     -- Heat product, other logic could be implemented here
                     o_HEATER <= '1';
                     -- in this case, simulation via a 1-Hz counter
                     Do_countdown <= '1';
                     -- Send BUSY code if another product is requested
-                    IF Recv_CMD_Product_Request THEN
-                        UART_TX_Send_flag <= '1';
-                        Send_status_enum <= BUSY;
+                    IF int_RX_CMD_Product THEN
+                        o_status_send <= '1';
+                        o_status <= BUSY;
                     END IF;
                 WHEN ORDER_CANCELLED =>
-                    Timer_clear <= '1';
+                    int_timer_clear <= '1';
                     -- Show status on 7-Segments
                     int_PRODUCT_TYPE := CANCEL;
                     -- Send current product was cancelled by any means (from FPGA or UART RX command)
-                    UART_TX_Send_flag <= '1';
-                    Send_status_enum <= CANCELLED;
-                    WHEN ORDER_FINISHED =>
+                    o_status_send <= '1';
+                    o_status <= CANCELLED;
+                WHEN ORDER_FINISHED =>
                     -- Send FINISHED code
-                    UART_TX_Send_flag <= '1';
-                    Send_status_enum <= FINISHED;
+                    o_status_send <= '1';
+                    o_status <= FINISHED;
                 WHEN OTHERS =>
                     NULL;
             END CASE;
--- SED_FPGA_Project/proyecto/proyecto.srcs/sources_1/telecom_entity.vhd
+++ SED_FPGA_Project/proyecto/proyecto.srcs/sources_1/telecom_entity.vhd
@@ -4,30 +4,118 @@ USE IEEE.numeric_std.ALL;
 
 USE WORK.MACHINE_COMMON.ALL;
 
--- Input + 3 secs; 2 LSBs mean the type of product
-ENTITY UART_RX_COMMAND_TRANSLATION IS
-    PORT (
-        i8_rx_code : IN BYTE;
-        o_IS_CANCEL_CMD : INOUT STD_ULOGIC;
-        o_product_type : OUT ProductType;
-        o8_converted_secs : OUT BYTE
+ENTITY COMMUNICATION_ENTITY IS
+    GENERIC (
+        g_CLK_FREQ : POSITIVE := 100_000_000;
+        g_UART_FREQ : POSITIVE := 10_000
     );
-END ENTITY UART_RX_COMMAND_TRANSLATION;
+    PORT (
+        i_CLK : IN STD_ULOGIC;
+        i_RESET_N : IN STD_ULOGIC;
 
-ARCHITECTURE RTL OF UART_RX_COMMAND_TRANSLATION IS
-    SIGNAL int_ORDER_LSBs : STD_ULOGIC_VECTOR(1 DOWNTO 0) := "00";
-    SIGNAL int_ORDER_MSBs : STD_ULOGIC_VECTOR(i8_rx_code'HIGH DOWNTO 2) := (OTHERS => '0');
+        i_serial_rx : IN STD_ULOGIC;
+        o_serial_tx : OUT STD_ULOGIC;
+
+        i_status : IN MachineStatus;
+        i_status_send : IN STD_ULOGIC;
+
+        o_cmd_received : OUT STD_ULOGIC;
+        o_cmd_cancel : INOUT STD_ULOGIC;
+        o_cmd_product : OUT STD_ULOGIC;
+        o_product_type : OUT ProductType;
+        o8_converted_secs : OUT BYTE; -- Input byte 6 MSBs + 3 secs; 2 LSBs mean the type of product
+        o_RX_brk : OUT STD_ULOGIC
+    );
+END ENTITY COMMUNICATION_ENTITY;
+
+ARCHITECTURE comms_ent_arch OF COMMUNICATION_ENTITY IS
+    --! Edge detector for i_status_send
+    COMPONENT EDGEDTCTR IS
+        PORT (
+            CLK, RST_N : IN STD_ULOGIC;
+            SYNC_IN : IN STD_ULOGIC;
+            EDGE : OUT STD_ULOGIC
+        );
+    END COMPONENT EDGEDTCTR;
+
+    --! UART
+    COMPONENT fluart IS -- Credits to https://github.com/marcj71/fluart
+        GENERIC (
+            CLK_FREQ : INTEGER := 50_000_000; -- main frequency (Hz)
+            SER_FREQ : INTEGER := 115200; -- bit rate (bps), any number up to CLK_FREQ / 2
+            BRK_LEN : INTEGER := 10 -- break duration (tx), minimum break duration (rx) in bits
+        );
+        PORT (
+            clk : IN STD_ULOGIC;
+            reset : IN STD_ULOGIC;
+
+            rxd : IN STD_ULOGIC;
+            txd : OUT STD_ULOGIC;
+
+            tx_data : IN STD_ULOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+            tx_req : IN STD_ULOGIC;
+            tx_brk : IN STD_ULOGIC;
+            tx_busy : OUT STD_ULOGIC;
+            tx_end : OUT STD_ULOGIC;
+            rx_data : OUT STD_ULOGIC_VECTOR(7 DOWNTO 0);
+            rx_data_valid : OUT STD_ULOGIC;
+            rx_brk : OUT STD_ULOGIC;
+            rx_err : OUT STD_ULOGIC
+        );
+    END COMPONENT fluart;
+    -- Signals
+    SIGNAL int_UART_RX_DATA : BYTE := (OTHERS => '0');
+    SIGNAL int_UART_TX_pulse : STD_ULOGIC := '0';
+    SIGNAL int_Send_status_byte : STD_ULOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+
+    SIGNAL int_RCV_CMD_LSBs : STD_ULOGIC_VECTOR(1 DOWNTO 0) := "00";
+    SIGNAL int_RCV_CMD_MSBs : STD_ULOGIC_VECTOR(int_UART_RX_DATA'HIGH DOWNTO 2) := (OTHERS => '0');
 BEGIN
-    int_ORDER_LSBs <= i8_rx_code(int_ORDER_LSBs'HIGH DOWNTO 0);
-    int_ORDER_MSBs <= i8_rx_code(i8_rx_code'HIGH DOWNTO int_ORDER_LSBs'HIGH + 1);
-    o_IS_CANCEL_CMD <= '1' WHEN UNSIGNED(i8_rx_code) = 255 ELSE
+    --! Edge detector, send data pulse
+    Inst00_UART_TX_REQ_PULSE : EDGEDTCTR
+    PORT MAP(
+        CLK => i_CLK,
+        RST_N => i_RESET_N,
+        SYNC_IN => i_status_send,
+        EDGE => int_UART_TX_pulse
+    );
+
+    --! UART instantiation
+    int_Send_status_byte <= MachineStatus2Byte(i_status);
+    Inst00_uart : fluart
+    GENERIC MAP(
+        CLK_FREQ => g_CLK_FREQ,
+        SER_FREQ => g_UART_FREQ
+    )
+    PORT MAP(
+        clk => i_CLK,
+        reset => NOT i_RESET_N,
+
+        rxd => i_serial_rx,
+        txd => o_serial_tx,
+
+        tx_data => int_Send_status_byte,
+        tx_req => int_UART_TX_pulse,
+        tx_brk => '0',
+        tx_busy => OPEN,
+        tx_end => OPEN,
+
+        rx_data => int_UART_RX_DATA,
+        rx_data_valid => o_cmd_received,
+        rx_brk => o_RX_brk,
+        rx_err => OPEN
+    );
+    int_RCV_CMD_LSBs <= int_UART_RX_DATA(int_RCV_CMD_LSBs'HIGH DOWNTO 0);
+    int_RCV_CMD_MSBs <= int_UART_RX_DATA(int_UART_RX_DATA'HIGH DOWNTO int_RCV_CMD_LSBs'HIGH + 1);
+    o_cmd_cancel <= '1' WHEN UNSIGNED(int_UART_RX_DATA) = 255 ELSE
         '0';
-    o8_converted_secs <= int_ORDER_MSBs & "11";
+    o_cmd_product <= NOT o_cmd_cancel;
+    o8_converted_secs <= int_RCV_CMD_MSBs & "11";
     o_product_type <=
-        CANCEL WHEN o_IS_CANCEL_CMD = '1' ELSE
-        COFFEE WHEN int_ORDER_LSBs = "00" ELSE
-        TEA WHEN int_ORDER_LSBs = "01" ELSE
-        MILK WHEN int_ORDER_LSBs = "10" ELSE
-        CHOCOLAT WHEN int_ORDER_LSBs = "11" ELSE
+        CANCEL WHEN o_cmd_cancel = '1' ELSE
+        COFFEE WHEN int_RCV_CMD_LSBs = "00" ELSE
+        TEA WHEN int_RCV_CMD_LSBs = "01" ELSE
+        MILK WHEN int_RCV_CMD_LSBs = "10" ELSE
+        CHOCOLAT WHEN int_RCV_CMD_LSBs = "11" ELSE
         NONE;
-END RTL; -- RTL
+END ARCHITECTURE comms_ent_arch;
